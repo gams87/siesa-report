f252c4baacb5e4202504d3bd00f1c9d01f105ec9
Alejandro Morera <galejandromorera@gmail.com>
2025-11-14 23:04:43 -0600

    static files

--- src/core/app/report/settings.py
+++ src/core/app/report/settings.py
@@ -177,13 +177,10 @@ USE_TZ = True
 
 MEDIA_ROOT = BASE_DIR / "media"
 MEDIA_URL = "/media/"
+
 STATIC_URL = "/static/"
-
-if not DEBUG:
-    STATIC_ROOT = BASE_DIR / "static"
-else:
-    STATICFILES_DIRS = (os.path.join(BASE_DIR, "static/"),)
-
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
